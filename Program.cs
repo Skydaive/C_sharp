638cf116ffa41b88b8a2a6b5b8cfc0891f7401d8
Skydaive <132383894+Skydaive@users.noreply.github.com>
2023-05-29 22:32:53 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,91 +1,105 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает
-// два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿
+//Задание 10.
+// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
 
 // Console.WriteLine("Введите число");
+
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// if (num>=100)
+// {
+//    num/=10;
+// }
+// int num2=num%10;
+// Console.WriteLine(num2);
+// ________________________________________________________
+
+//Задание 13.
+// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = ReadLine("Введите число ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+int ReadLine(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+if (b < 3)
+{
+ Console.Write("Третьей цифры нет");
+}
+ else
+{
+ int c = 1;
+ for (int i = b; i > 3; i--)
+{
+ c = c * 10;
+}
+ result = (a / c) % 10;
+}
+return result;
+}
+
+
+// ____________________________________________________
+
+// Задача 15.
+// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Какой сегодня день, выходной или нет?");
 // int num = Convert.ToInt32(Console.ReadLine());
 
-// if (num == 5)
+// if (num == 1)
 // {
-//     Console.WriteLine("min");
-// }
-// else if (num == 7)
-// {
-//     Console.WriteLine("max");
+//     Console.WriteLine("Нет - Понедельник");
 // }
 // else if (num == 2)
 // {
-//     Console.WriteLine("min");
+//     Console.WriteLine("Нет-Вторник");
 // }
-// else if (num == 10)
+// else if (num == 3)
 // {
-//     Console.WriteLine("max");
+//     Console.WriteLine("Нет - Среда");
 // }
-// else if (num == -9)
+// else if (num == 4)
 // {
-//     Console.WriteLine("min");
+//     Console.WriteLine("Нет - Четверг");
 // }
-// else if (num == -3)
+// else if (num == 5)
 // {
-//     Console.WriteLine("max");
+//     Console.WriteLine("Нет - Пятница");
+// }
+// else if (num == 6)
+// {
+//     Console.WriteLine("Да - Суббота - выходной");
+// }
+// else if (num == 7)
+// {
+//     Console.WriteLine("Да - Воскресенье - выходной");
 // }
 // else
 // {
-//     Console.WriteLine("Требуется выбрать одно число из условия");
-
+//     Console.WriteLine("Введите число от 1 до 7");
 // }
-// ___________________________________________________________________________
-
-// Задача 4: Напишите программу, которая принимает
-// на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
-
-// Console.WriteLine("Введите число");
-// int a = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите число");
-// int b = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите число");
-// int c = Convert.ToInt32(Console.ReadLine());
-
-// int max = a;
-// if (max < b) max = b;
-// if (max < c) max = c;
-// System.Console.WriteLine("Максимальное из чисел = " + max);
-
-// ________________________________________________________________________________
-
-// Задача 6: Напишите программу, которая на вход принимает
-// число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
-
-// Console.WriteLine("Введите число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// if (a % 2 == 0)
-//     System.Console.WriteLine("Число : " + (a) + " четное");
-// else
-//     System.Console.WriteLine($"Число : {a} нечетное");
-
-// ___________________________________________________________________________________
-
-// Задача 8: Напишите программу, которая на вход принимает
-// число (N), а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
-
-// Console.WriteLine("Введите число");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// for (int i = 1; i <= n; i++)
-// {
-//     if (i % 2 == 0)
-//         System.Console.Write(i + ",");
-// }
-
